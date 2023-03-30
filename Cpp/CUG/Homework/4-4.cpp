512cebabdb574c5e34ebfe73f20c1b23440d83c3
ChanningTong <Channing_TongCN@outlook.com>
2023-03-31 07:43:46 +0800

    MIM/ICM problem

--- Cpp/CUG/Homework/4-4.cpp
+++ Cpp/CUG/Homework/4-4.cpp
@@ -1,12 +1,12 @@
-/*** 
+/***
  * @Author: ChanningTong Channing_TongCN@outlook.com
  * @Date: 2023-03-25 22:01:58
  * @LastEditors: ChanningTong Channing_TongCN@outlook.com
  * @LastEditTime: 2023-03-25 22:34:42
  * @FilePath: \undefinedc:\Users\Administrator\GitHub\MyCoding\Cpp\CUG\Homework\4-4.cpp
- * @Description: 
+ * @Description:
  * @
- * @Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ * @Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
  */
 #include<iostream>
 #include<cstring>
@@ -22,18 +22,16 @@ struct Account{
 };
 double Power(double base, double exp,int year){
     double result = base,rate=exp;
-    for(int i=1;i<year;i++){
-        result*=(1+rate);
+    for(int i=0;i<year;i++){
+        result=result*(1+rate);
         rate*=decrease;
-    //    cout<<result<<'\t'<<rate<<endl;
-    //    system("pause");
     }
     return result;
 }
 int main(){
     Account *p = new Account;
     cin>> p->name >> p->deposit >> p->balanceRate >> p->year;
-    (p->balanceRate)/=100;
+    (p->balanceRate)/=100; //转换为小数
     double balance = Power(p->deposit,p->balanceRate, p->year);
     cout << p->name << '\t' <<balance;
     cin.get();
