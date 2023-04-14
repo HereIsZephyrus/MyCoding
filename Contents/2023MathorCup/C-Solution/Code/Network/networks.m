4244f10272baca64f02ada03bfb0112897ee73be
ChanningTong <Channing_TongCN@outlook.com>
2023-04-14 15:13:26 +0800

    Backup3

--- Contents/2023MathorCup/C-Solution/Code/Network/networks.m
+++ Contents/2023MathorCup/C-Solution/Code/Network/networks.m
@@ -5,8 +5,8 @@ ind_start=1;
 while(ind_start<=n)
     ind_end=During(ind_start,Date,n);
     Matrix=makeAdj(Gragh(ind_start:ind_end,:),num);
-    [starts,endsWith]=;
-    Painting(Gragh);
+    [starts,ends]=checkterm(Matrix,num);
+    Painting(Matrix,starts);
     ind_start=ind_end+1;
 end
 clear ind_start ind_end
@@ -17,18 +17,17 @@ function Matrix=makeAdj(edges,num)
     n=length(edges);
     for i=1:n
         Matrix(edges(i,1),edges(i,2))=Matrix(edges(i,1),edges(i,2))+1;
-        if (Matrix(edges(i,1),edges(i,2))==1)
-            num_connect=num_connect+1;
-        end
+        %if (Matrix(edges(i,1),edges(i,2))==1)
+        %    num_connect=num_connect+1;
+        %end
     end
 end
-function [starts,ends]=checkterm(subGragh)
-    
+function [starts,ends]=checkterm(Matrix,num)   
     num_start=0;
     for i=1:num
         if (any(Matrix(:,i))==0)%i点没有出度
             num_start=num_start+1;
-            ind_start(1,num_start)=i;
+            starts(1,num_start)=i;
         end
     end
     clear i
@@ -36,11 +35,11 @@ function [starts,ends]=checkterm(subGragh)
     for i=1:num
         if (any(Matrix(i,:))==0)%i点没有入度
             num_end=num_end+1;
-            ind_end(1,num_end)=i;
+            ends(1,num_end)=i;
         end
     end
 end
-function Painting()
+function Painting(Matrix,starts)
     
 end
 function ind=During(init,Date,n)
