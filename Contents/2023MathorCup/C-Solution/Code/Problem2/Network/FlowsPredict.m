04609b5c37205ec7b3e45703f1bb0b4298293c34
ChanningTong <Channing_TongCN@outlook.com>
2023-04-16 11:05:45 +0800

    backup-2023-4-16-11:05

--- Contents/2023MathorCup/C-Solution/Code/Problem2/Network/FlowsPredict.m
+++ Contents/2023MathorCup/C-Solution/Code/Problem2/Network/FlowsPredict.m
@@ -9,22 +9,17 @@ nearFlows=Flows(:,365*2-61+1:365*2);
 
 clear i
 len_pre=31;%预测长度
-Predict=zeros(4,len_pre);
-str={'左节点一月预测','右节点一月预测','左节点的右节点一月预测','右节点的左节点一月预测'};
 err=zeros(1,4);
 Predict=cell(1,4);
-figure(2);
 for cate=1:4
-    subplot(4,1,cate);
-    hold on
     for i=1:length(E{cate})
         edge=E{cate}(i);
         Data=Time_var(edge,365*2-61+1:365*2);
         Data=filloutliers(Data,"linear");
         len=length(Data);
         tmp_data=Data;
+        len_block=40;%滑块长度
         for j=1:len_pre
-            len_block=40;%滑块长度
             len=len+1;
             try
             model = arima(5,0,1);
@@ -38,19 +33,9 @@ for cate=1:4
                 tmp_data(len)=Average(tmp_data,len_block);
             end
         end
-        %bias=randn(1,31)*std(result{i})*0.5+exp(1:0.1:4);bias(1)=bias(1)+5;
         Predict{cate}(i,:)=tmp_data(len-len_pre+1:len);
-        %plot([datetime(2022,11,12:30),datetime(2022,12,1:31)],Data(end-50+1:end),'b-');
-        %plot(41:81,[tmp_data(end-len_proving+1:end),Predict(i,:)],'r+-');
-        plot(datetime(2023,1,1:31),Predict{cate}(i,:));
-        %datetick('x','mm-dd');    
-        
         clear len a b edge
     end
-    xlim([datetime("2023-01-01") datetime("2023-01-31")]);
-    ylabel("货量");
-    title(str{cate});
-    hold off
 end
 PredictFlows=Predict;
 clear Predict
@@ -61,7 +46,4 @@ clear ans
 function y_pre=Average(y,n)
     m=length(y);
     y_pre=sum( y(m-n+1:m) ) / n;
-end
-function str=Node(node)
-    str=strcat("DC",num2str(node));
 end
