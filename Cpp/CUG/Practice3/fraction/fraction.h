75ad9abfff08f4bb895672bc5fd6d823a571f718
ChanningTong <Channing_TongCN@outlook.com>
2023-04-23 18:54:20 +0800

    feat: add Prctice3:fracton

---
+++ Cpp/CUG/Practice3/fraction/fraction.h
@@ -0,0 +1,163 @@
+/***
+ * @Author: ChanningTong Channing_TongCN@outlook.com
+ * @Date: 2023-04-22 17:48:52
+ * @LastEditors: ChanningTong Channing_TongCN@outlook.com
+ * @LastEditTime: 2023-04-23 17:09:26
+ * @FilePath: \Practice3\fraction\fraction.h
+ * @Description:
+ * @
+ * @Copyright (c) 2023 by ChanningTong, All Rights Reserved.
+ */
+#pragma once
+#include <iostream>
+class fraction{
+    private:
+        struct Number{
+            int numerator; // 分子
+            int denominator; // 分母
+        }value;
+        int gcd(int a, int b) const{
+            return b == 0 ? a : gcd(b, a % b);
+        } // 求最大公约数
+    protected:
+        Number simplify(Number x){
+            int m = gcd(x.numerator, x.denominator);
+            x.numerator /= m;
+            x.denominator /= m;
+            return x;
+        } // 约分
+        Number standlize(double num) const{
+            Number Res={0,1};
+            for (int counting=0; ((counting<9) && (num !=int(num))); ++counting){
+                num *= 10;
+                Res.denominator *= 10;
+            }
+            Res.numerator = int(num);
+        //    std::cout << num << std::endl;
+        //    std::cout << Res.numerator<<'/'<< Res.denominator<< std::endl;
+        //    system("pause");
+            return Res;
+        } // 标准化(分子分母都为整数
+    public:
+        fraction(int num = 0, int den = 1): value{num, den}{
+            value = std::move(simplify(value));
+        };
+        fraction(double num):value{0,1}{
+            value=std::move(simplify(standlize(num)));
+        }
+        fraction(double num, double den):value{0,1}{
+            value=std::move(simplify(standlize(num/den)));
+        }
+        ~fraction()=default;
+        friend fraction operator+(const fraction &f1, const fraction &f2){
+            fraction Res = {0, 1};
+            Res.value.numerator = (long long)f1.value.numerator * f2.value.denominator + (long long)f2.value.numerator * f1.value.denominator;
+            Res.value.denominator =f1.value.denominator* f2.value.denominator;
+            Res.value=Res.simplify(Res.value);
+            return Res;
+        }
+        friend fraction operator-(const fraction &f1, const fraction &f2){
+            fraction Res = {0, 1};
+            Res.value.numerator = (long long)f1.value.numerator * f2.value.denominator - (long long)f2.value.numerator * f1.value.denominator;
+            Res.value.denominator = f1.value.denominator* f2.value.denominator;
+            Res.value = Res.simplify(Res.value);
+            return Res;
+        }
+        friend fraction operator*(const fraction &f1, const fraction &f2){
+            fraction Res = {0, 1};
+            Res.value.numerator = (long long)f1.value.numerator * f2.value.numerator;
+            Res.value.denominator =f1.value.denominator * f2.value.denominator;
+            Res.value = Res.simplify(Res.value);
+            return Res;
+        }
+        friend fraction operator/(const fraction &f1, const fraction &f2){
+            fraction Res = {0, 1};
+            Res.value.numerator = (long long)f1.value.numerator * f2.value.denominator;
+            Res.value.denominator =f1.value.denominator * f2.value.numerator;
+            Res.value = Res.simplify(Res.value);
+            return Res;
+        }
+
+        friend fraction operator+(const double &d,const fraction &f){
+            fraction tmp(d );
+            return tmp + f;
+        }
+        friend fraction operator-(const double &d,const fraction &f){
+            fraction tmp(d );
+            return tmp - f;
+        }
+        friend fraction operator*(const double &d,const fraction &f){
+            fraction tmp(d );
+            return tmp * f;
+        }
+        friend fraction operator/(const double &d,const fraction &f){
+            fraction tmp(d );
+            return tmp / f;
+        }
+        friend fraction operator+(const int &d,const fraction &f){
+            fraction tmp(d, 1);
+            return tmp + f;
+        }
+        friend fraction operator-(const int &d,const fraction &f){
+            fraction tmp(d, 1);
+            return tmp - f;
+        }
+        friend fraction operator*(const int &d,const fraction &f){
+            fraction tmp(d, 1);
+            return tmp * f;
+        }
+        friend fraction operator/(const int &d,const fraction &f){
+            fraction tmp(d, 1);
+            return tmp / f;
+        }
+        friend fraction operator+(const fraction &f,const double &d){
+            fraction tmp(d);
+            return tmp + f;
+        }
+        friend fraction operator-(const fraction &f,const double &d){
+            fraction tmp(d);
+            return f-tmp;
+        }
+        friend fraction operator*(const fraction &f,const double &d){
+            fraction tmp(d);
+            return tmp * f;
+        }
+        friend fraction operator/(const fraction &f,const double &d){
+            fraction tmp(d);
+            return f/tmp;
+        }
+        friend fraction operator+(const fraction &f,const int &d){
+            fraction tmp(d, 1);
+            return tmp + f;
+        }
+        friend fraction operator-(const fraction &f,const int &d){
+            fraction tmp(d, 1);
+            return f-tmp;
+        }
+        friend fraction operator*(const fraction &f,const int &d){
+            fraction tmp(d, 1);
+            return tmp * f;
+        }
+        friend fraction operator/(const fraction &f,const int &d){
+            fraction tmp(d, 1);
+            return f/tmp;
+        }
+
+        friend std::istream& operator>>(std::istream &is, fraction &f){
+            std::cout<<"Please input the numerator and denominator:"<<std::endl;
+            double num, den;
+            is >> num >> den;
+            f = fraction(num, den);
+            return is;
+        }
+        friend std::ostream& operator<<(std::ostream &os, const fraction &f){
+            os << f.value.numerator << "/" << f.value.denominator;
+            return os;
+        }
+        operator double() const{
+            return double(value.numerator)/double(value.denominator);
+        }
+        operator int() const{
+            return int(value.numerator / value.denominator);
+        }
+};
---
+++ Cpp/CUG/Practice3/fraction/main.cpp
@@ -0,0 +1,35 @@
+/***
+ * @Author: ChanningTong Channing_TongCN@outlook.com
+ * @Date: 2023-04-22 17:47:49
+ * @LastEditors: ChanningTong Channing_TongCN@outlook.com
+ * @LastEditTime: 2023-04-22 17:47:59
+ * @FilePath: \Practice3\fraction\main.cpp
+ * @Description:
+ * @分数类的主程序
+ * @Copyright (c) 2023 by ChanningTong, All Rights Reserved.
+ */
+#include <iostream>
+#include "fraction.h" // class fraction
+
+using namespace std;
+int main(){
+    fraction f1(1, 2); // 表示1/2
+    fraction f2(1.2, 0.5); // 表示1.2/0.5
+    fraction f3(0.6); //转换为分数3/5
+    fraction f4;
+    fraction f5 = f3; //拷贝构造
+    cin >> f4; //输入分子，分母，并简化
+    cout << f1 + f2 << endl;
+    cout << f1 - f2 << endl;
+    cout << f1 * f2 << endl;
+    cout << f1 / f2 << endl;
+    cout << f3 + 0.5 << endl; // -*/都支持
+    cout << 0.5 + f3 << endl; // -*/都支持
+    cout << f4 + 1 << endl;
+    cout << 1 + f4 << endl;
+    cout << double(f5) << endl;
+    cout << double(f5) + 0.5 << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
