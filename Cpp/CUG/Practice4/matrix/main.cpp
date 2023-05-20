5b326f99354d43cf293f2de3ee180a444e7cbc56
ChanningTong <Channing_TongCN@outlook.com>
2023-05-20 11:53:14 +0800

    feat:Finish all the Pratice!

--- Cpp/CUG/Practice4/matrix/main.cpp
+++ Cpp/CUG/Practice4/matrix/main.cpp
@@ -2,17 +2,7 @@
  * @Author: ChanningTong Channing_TongCN@outlook.com
  * @Date: 2023-04-30 20:07:40
  * @LastEditors: ChanningTong Channing_TongCN@outlook.com
- * @LastEditTime: 2023-05-19 18:37:27
- * @FilePath: \matrix\main.cpp
- * @Description:
- * @
- * @Copyright (c) 2023 by ChanningTong, All Rights Reserved.
- */
-/***
- * @Author: ChanningTong Channing_TongCN@outlook.com
- * @Date: 2023-04-30 20:07:40
- * @LastEditors: ChanningTong Channing_TongCN@outlook.com
- * @LastEditTime: 2023-04-30 21:22:58
+ * @LastEditTime: 2023-05-20 10:30:12
  * @FilePath: \matrix\main.cpp
  * @Description:
  * @
@@ -23,9 +13,10 @@
 #include"matrix.h"
 
 int main(){
-    Matrix<double,2,3> m;
-    Matrix<int,3,3> m1,m2;
+    Matrix<double> m(2,3);
+    Matrix<int> m1(3,3),m2(3,3);
     std::cin>>m1>>m2;
+    m1+m2;
     std::cout<<m1+m2<<std::endl;
     std::cout<<m1-m2<<std::endl;
     std::cout<<m1*m2<<std::endl;
--- Cpp/CUG/Practice4/matrix/matrix.h
+++ Cpp/CUG/Practice4/matrix/matrix.h
@@ -15,70 +15,91 @@
 #include<cstring>
 #include<cstddef>
 
-template<class Type, size_t Row, size_t Col>
+template<class Type>
 class Matrix{
 private:
     Type** mat=nullptr;//Value
-    const int row=Row;
-    const int col=Col;
+    int row{1};
+    int col{1};
+    bool existData{false};
 public:
     Type** Generate(int,int,const char *);
-    Matrix(){
+    Matrix(int rows,int cols):row(rows),col(cols){
         mat=Generate(row,col,"zeros");
+        existData=true;
     }
-    Matrix(const char *s){
+    Matrix(int rows, int cols,const char *s) :row(rows), col(cols) {
         mat=Generate(row,col,s);
+        existData=true;
     }
     ~Matrix(){
         for(int i=0;i<row;++i)
             delete[] mat[i];
         delete[] mat;
+        existData=false;
     }//防止内存溢出
-    int getRow() const{return row;}
-    int getCol() const{return col;}
-    Matrix& operator+(const Matrix& m){
+    Matrix& operator=(const Matrix& other){
+        if (this==&other)
+            return *this;
+        for(int i=0;i<row;++i)
+            delete[] mat[i];
+        delete[] mat;
+        existData=false;
+        row=other.row;
+        col=other.col;
+        mat=Generate(row,col,"zeros");
+        for(int i=0;i<row;++i)
+            for(int j=0;j<col;++j)
+                mat[i][j]=other.mat[i][j];
+        return *this;
+    }
+
+    int getRow() const { return row; }
+    int getCol() const { return col; }
+    Matrix operator+(const Matrix& m) const{
         std::cout<<"Plus"<<std::endl;
         if (row!=m.row||col!=m.col){
             std::cout<<"Error: Matrix size mismatch"<<std::endl;
             return *this;
         }
+        Matrix res(row,col);
         for(int i=0;i<row;++i)
             for(int j=0;j<col;++j)
-                mat[i][j]=static_cast<Type>(mat[i][j]+m.mat[i][j]);
-        return *this;
+                res.mat[i][j]=static_cast<Type>(mat[i][j]+m.mat[i][j]);
+        return res;
     }
-    Matrix& operator-(const Matrix& m){
+    Matrix operator-(const Matrix& m) const{
         std::cout << "Minus" << std::endl;
         if (row!=m.row||col!=m.col){
             std::cout<<"Error: Matrix size mismatch"<<std::endl;
             return *this;
         }
+        Matrix res(row,col);
         for(int i=0;i<row;++i)
             for(int j=0;j<col;++j)
-                mat[i][j] = static_cast<Type> (mat[i][j] - m.mat[i][j]);
-        return *this;
+                res.mat[i][j] = static_cast<Type> (mat[i][j] - m.mat[i][j]);
+        return res;
     }
-    Matrix& operator*(const Matrix& m){
+    Matrix operator*(const Matrix& m) const{
         std::cout << "Multiply" << std::endl;
         if (col!=m.row){
             std::cout<<"Error: Matrix size mismatch"<<std::endl;
             return *this;
         }
-        Matrix tmp=*this;
-        tmp.col=m.getCol();
-        tmp.mat=Generate(row,m.col,"zeros");
+        Matrix res(row,m.col);
         for(int i=0;i<row;++i)
             for(int j=0;j<m.col;++j)//矩阵的最终大小为row*m.col
                 for(int k=0;k<col;++k)
-                    tmp.mat[i][j] += static_cast<Type> (mat[i][k] * m.mat[k][j]);
-        return tmp;
+                    res.mat[i][j] += static_cast<Type> (mat[i][k] * m.mat[k][j]);
+        return res;
     }
-    Matrix& operator*(const double d){
+    Matrix operator*(const double d) const{
         std::cout << "Multiply" << std::endl;
+        Matrix res(row,col);
         for(int i=0;i<row;++i)
             for(int j=0;j<col;++j)
-                mat[i][j]*=d;
-        return *this;
+                res.mat[i][j]= static_cast<Type>(mat[i][j]*d);
+        return res;
     }
     friend std::ostream& operator<<(std::ostream& os,const Matrix& m){
         for(int i=0;i<m.row;++i){
@@ -89,10 +110,9 @@ public:
         return os;
     }
     friend std::istream& operator>>(std::istream& is,Matrix& m){
-        std::cout<<"Please input the matrix:"<<std::endl;
-        m.mat = new Type *[m.row];
-        for (int i = 0; i < m.row; ++i)
-            m.mat[i] = new Type[m.col];
+        std::cout<<"Please input the matrix("<<m.getRow()<<'*'<<m.getCol()<<"):"<<std::endl;
+        if (!m.existData)
+            m.Generate(m.row,m.col,"zeros");
         for(int i=0;i<m.row;++i)
             for(int j=0;j<m.col;++j)
                 is>>m.mat[i][j];
@@ -100,19 +120,24 @@ public:
     }
 };
 
-template <class Type, size_t Row, size_t Col>
-Type ** Matrix<Type,Row,Col>::Generate(int col, int row, const char *s){
+template <class Type>
+Type ** Matrix<Type>::Generate(int col, int row, const char *s){
+    if (existData) {
+        for(int i=0;i<row;++i)
+            delete[] mat[i];
+        delete[] mat;
+    }
     Type** tmpMat = new Type *[row];
     for (int i = 0; i < row; ++i)
         tmpMat[i] = new Type[col];
-    if(std::strcmp(s,"ones")==0)//生成全1矩阵
-        for(int i=0;i<row;++i)
-            for(int j=0;j<col;++j)
-                tmpMat[i][j]=static_cast<Type>(1);
-    else if(std::strcmp(s,"zeros")==0)//生成零矩阵
+    if(std::strcmp(s,"zeros")==0)//生成零矩阵
         for(int i=0;i<row;++i)
             for(int j=0;j<col;++j)
                 tmpMat[i][j]=static_cast<Type>(0);
+    else if(std::strcmp(s,"ones")==0)//生成全1矩阵
+        for(int i=0;i<row;++i)
+            for(int j=0;j<col;++j)
+                tmpMat[i][j]=static_cast<Type>(1);
     else if(std::strcmp(s,"eye")==0)
         for(int i=0;i<row;i++)
             for(int j=0;j<col;j++)
@@ -124,5 +149,6 @@ Type ** Matrix<Type,Row,Col>::Generate(int col, int row, const char *s){
         std::cout<<"Error: Invalid String"<<std::endl;
         return nullptr;
     }
+    existData=true;
     return tmpMat;
 }
