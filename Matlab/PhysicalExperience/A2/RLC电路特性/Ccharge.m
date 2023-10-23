29ebfd1f98364c25b67e3759f6d6d204bc15f21d
ChanningTong <Channing_TongCN@outlook.com>
2023-10-23 23:04:41 +0800

    RLC电路特性
    物理实验

--- Matlab/PhysicalExperience/A2/RLC电路特性/Ccharge.m
+++ Matlab/PhysicalExperience/A2/RLC电路特性/Ccharge.m
@@ -1,2 +1,20 @@
-b1=sqrt(1/(C*L)-R*R/(4*L*L));
-It=@(a) sind(n1+a);
+clc;clear;close all;
+C=0.1e-6;
+R=[10e3,20e3,40e3];
+dV_half=[3.70,3.40,2.44];
+t=[700e-6,1.3e-3,2.1e-3];
+subplot(1,2,1);
+hold on
+xlabel("电容两端电压下降1/2使用的时间t/s");
+ylabel("串联电路电阻R/ \Omega");
+title("RC电路放电时间与电阻函数(C固定)");
+plot(t,R,'b*-');
+subplot(1,2,2);
+tt=linspace(min(t),max(t),100);
+vv=spline(t,dV_half,tt);
+plot(tt,vv*2,'g-');
+xlabel("电容两端电压下降1/2使用的时间t/s");
+ylabel("电阻两端电压/ V");
+title("RC电路放电时间与电容两端电压函数(C固定)");
+hold off
+
--- Matlab/PhysicalExperience/A2/RLC电路特性/RLCline.m
+++ Matlab/PhysicalExperience/A2/RLC电路特性/RLCline.m
@@ -1,10 +1,38 @@
 clc;clear;close all;
 f0=5020;    U0=5.88;
 f=[540.8,1034,2500,4000,6259,7587,8830,11044];
+omiga=2*pi*f;
 U=[0.208,1.02,1.36,3.52,3.64,2.24,1.68,1.16];
-phi=[-87.5,-85.2,-78.1,-53.7,52.5,68.5,72,80];
-R0=100;C=0.1;L=10; %参数设置 omega / nuf /mH
-R=phi*L*1i+1./(phi*C*1i)+R0;
-I=U./abs(R);
-I0=R0;
-plot(f,I,'b*-');
+phi_e=[-87.5,-85.2,-78.1,-53.7,52.5,68.5,72,80];
+R0=100;C=0.1e-6;L=10e-3; %参数设置
+
+% 理论计算
+R=abs(omiga*L*1i+1./(omiga*C*1i)+R0);
+R=[R(1:4),R0,R(5:8)];U=[U(1:4),U0,U(5:8)];f=[f(1:4),f0,f(5:8)];
+omiga=2*pi*f;
+phi_t=atand((omiga*L-1./(omiga*C))/R0);phi_e=[phi_e(1:4),0,phi_e(5:8)];
+dphi=phi_t-phi_e;vphi=var(dphi);
+%% 图像呈现
+subplot(1,2,1);
+hold on
+I=U./R;
+plot(f,I,'b*');
+ff=linspace(min(f),max(f),1000);
+ii=spline(f,I,ff);
+plot(ff,ii,'g-');
+xlabel("电流频率f/Hz");
+ylabel("电流I/I_{max}");
+title("RLC电路电流随频率变化关系");
+
+hold off
+subplot(1,2,2);
+hold on
+lgR=log(R);lgF=log(f);
+plot(lgF,lgR,'b*');
+lff=linspace(min(lgF),max(lgF),1000);
+lrr=spline(lgF,lgR,lff);
+plot(lff,lrr,'g-');
+xlabel("电流频率log_{10}f/Hz");
+ylabel("等效电阻log_{10}R/R_{总}");
+title("RLC电路总电阻与频率变化关系");
+hold off
